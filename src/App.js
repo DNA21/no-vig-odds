9cd1faa5d947e93e88d1e2d778ce3b34fcd93a71
Daniel Galuardi <dan.galuardi@gmail.com>
2023-05-04 14:56:32 -0400

    Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './theme';
 import { GlobalStyles } from './global';
@@ -17,18 +18,19 @@ function App() {
     const [theme, setTheme] = useState('light')
     const [games, setGames] = useState([])
 
-    const url = 'https://api.the-odds-api.com/v4/sports/baseball_mlb/odds?apiKey=980b2b89872e4cb9cc002b0442060d1e&regions=us&markets=h2h&dateFormat=iso&oddsFormat=american&bookmakers=fanduel'
+    // const url = 'https://api.the-odds-api.com/v4/sports/baseball_mlb/odds?apiKey=980b2b89872e4cb9cc002b0442060d1e&regions=us&markets=h2h&dateFormat=iso&oddsFormat=american&bookmakers=fanduel'
 
-    useEffect(() => {
-        axios.get(url).then((response) => {
-            setGames(response.data)
-            console.log(response.data)
-        })
-    }, [])
+    // useEffect(() => {
+    //     axios.get(url).then((response) => {
+    //         setGames(response.data)
+    //         console.log(response.data)
+    //     })
+    // }, [url])
 
 
     return (
         <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <GlobalStyles />
             <Header theme={theme} setTheme={setTheme} />
             <Routes>
                 <Route path='/' element={ <Home /> } />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,30 +17,34 @@ const Header = (props) => {
     const [menuOpen, setMenuOpen] = useState(false)
 
     const toggleTheme = () => {
-        theme === 'light' ? setTheme('dark') : setTheme('light')
+        (theme === 'light') ? setTheme('dark') : setTheme('light')
     }
 
     return (
-        <Container>
-            <Navbar>
-                <NavbarBrand>
+        <Container className='px-0'>
+            <Navbar className={`${(theme==='dark') ? 'navbar-dark' : 'navbar-light'} d-flex justify-content-between align-items-center shadow-lg rounded-4 mt-4 fw-bold bgSecondary`} expand='md' sticky='top'>
+                <NavbarBrand href='/'>
                     <h1>No Vig</h1>
                 </NavbarBrand>
 
-                <NavbarToggler />
-                <Collapse>
-                    <Nav>
-                        <NavItem>
-                            <NavLink>MLB</NavLink>
+
+                <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+                <Collapse isOpen={menuOpen} navbar className={`${menuOpen ? 'nav-collapse bgSecondary' : ''} container`}>
+                    <Nav className={`${(menuOpen===false) ? 'ms-auto d-flex align-items-center' : null} `} navbar>
+                        <NavItem className={`${menuOpen===false ? null : 'py-4' }`}>
+                            <Toggle theme={theme} toggleTheme={toggleTheme} />
                         </NavItem>
                         <NavItem>
-                            <NavLink>NBA</NavLink>
+                            <NavLink to='/mlb' onClick={() => setMenuOpen(!menuOpen)} className={`${(menuOpen===false) ? 'text-link px-4 bgButtonSecondary py-2 rounded-5 shadow-lg mx-2' : 'nav-link border-bottom py-5'}`}>MLB</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink>NFL</NavLink>
+                            <NavLink to='/nba' onClick={() => setMenuOpen(!menuOpen)} className={`${(menuOpen===false) ? 'text-link px-4 bgButtonSecondary py-2 rounded-5 shadow-lg mx-2' : 'nav-link border-bottom py-5'}`}>NBA</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink>NHL</NavLink>
+                            <NavLink to='/nfl' onClick={() => setMenuOpen(!menuOpen)} className={`${(menuOpen===false) ? 'text-link px-4 bgButtonSecondary py-2 rounded-5 shadow-lg mx-2' : 'nav-link border-bottom py-5'}`}>NFL</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink to='/nhl' onClick={() => setMenuOpen(!menuOpen)} className={`${(menuOpen===false) ? 'text-link px-4 bgButtonSecondary py-2 rounded-5 shadow-lg mx-2' : 'nav-link py-5'}`}>NHL</NavLink>
                         </NavItem>
                     </Nav>
                 </Collapse>
